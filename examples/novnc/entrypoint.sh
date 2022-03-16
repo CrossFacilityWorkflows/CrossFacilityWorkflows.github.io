f5fd4609300bffe267a2d2d448a98a918faf47f2
Johannes Blaschke <johannes@blaschke.science>
2022-03-16 07:46:32 -0700

    add novnc example

--- examples/novnc/entrypoint.sh
+++ examples/novnc/entrypoint.sh
@@ -1,6 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -ex
 
-# export the utilities definitions to subshells
-set -o allexport
+RUN_FLUXBOX=${RUN_FLUXBOX:-yes}
+RUN_XTERM=${RUN_TERMINATOR:-yes}
 
-exec "$@"
+case $RUN_FLUXBOX in
+  false|no|n|0)
+    rm -f /srv/conf.d/fluxbox.conf
+    ;;
+esac
+
+case $RUN_TERMINATOR in
+  false|no|n|0)
+    rm -f /srv/conf.d/terminator.conf
+    ;;
+esac
+
+exec supervisord -c /srv/supervisord.conf
